1674bb862985e3632da1063c207e2b85f51bdbbd
unknown <manavlohar414141@gmail.com>
2025-09-16 01:03:52 +0530

    Add shadcn ui library, create sign up page and implement sign up api for posting user data
    Signed-off-by: unknown <manavlohar414141@gmail.com>

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,29 +1,5 @@
-"use client";
+const page = () => {
+  return <div>Sign in page</div>;
+};
 
-import { useSession, signIn, signOut } from "next-auth/react";
-
-export default function Component() {
-  const { data: session } = useSession();
-
-  if (session) {
-    return (
-      <>
-        <p>Signed in as {session.user.email}</p>
-
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-
-  return (
-    <>
-      Not signed in <br />
-      <button
-        className="bg-blue-900 text-slate-200 border border-slate-50 py-1 px-3 m-2 rounded-md cursor-pointer"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
-  );
-}
+export default page;
